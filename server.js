3293a9eec4eafce23e4a705224012756fd7afe11
cedmondson98311 <cedmondson98311@gmail.com>
2017-04-22 09:34:44 -0700

    added endpoints for each page

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+app.use(express.static('views'));
 app.listen(process.env.PORT || 8080);
+
+app.get('/user/logs', (req, res) => {
+	res.sendFile(__dirname + '/views/view_logs.html')
+});
+
+app.get('/user/start', (req, res) => {
+	res.sendFile(__dirname + '/views/start_workout.html')
+});
+
+app.get('/user/workout', (req, res) => {
+	res.sendFile(__dirname + '/views/workout.html')
+});
+
+app.get('/user/create', (req, res) => {
+	res.sendFile(__dirname + '/views/create.html')
+});
 
 module.exports = {app};
