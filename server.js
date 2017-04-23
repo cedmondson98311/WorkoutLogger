171b07cb492947b7c27359e69f6599b205c73c6e
cedmondson98311 <cedmondson98311@gmail.com>
2017-04-22 17:41:10 -0700

    fixed index.html

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ var app = express();
 app.use(express.static('views'));
 app.listen(process.env.PORT || 8080);
 
+app.get('/',(req, res) => {
+	res.sendFile(__dirname + '/views/html/index.html')
+});
+
 app.get('/user/logs', (req, res) => {
-	res.sendFile(__dirname + '/views/view_logs.html')
+	res.sendFile(__dirname + '/views/html/view_logs.html')
 });
 
 app.get('/user/start', (req, res) => {
-	res.sendFile(__dirname + '/views/start_workout.html');
+	res.sendFile(__dirname + '/views/html/start_workout.html');
 });
 
 app.get('/user/workout', (req, res) => {
-	res.sendFile(__dirname + '/views/workout.html')
+	res.sendFile(__dirname + '/views/html/workout.html')
 });
 
 app.get('/user/create', (req, res) => {
-	res.sendFile(__dirname + '/views/create.html')
+	res.sendFile(__dirname + '/views/html/create.html')
 });
 
 module.exports = {app};
