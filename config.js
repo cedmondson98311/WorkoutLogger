d1a9073c61b32089956c9bb544af880cf7bae97e
cedmondson98311 <cedmondson98311@gmail.com>
2017-05-10 20:29:14 -0700

    fixed database URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/workout-logger-db';
+                      'mongodb://cory:1101762@ds115671.mlab.com:15671/workout_logger';
 exports.PORT = process.env.PORT || 8080;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
 							global.TEST_DATABASE_URL ||
