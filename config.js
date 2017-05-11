8176912a8fb5aab996bef9ed1b2643b57dd8755e
cedmondson98311 <cedmondson98311@gmail.com>
2017-05-10 20:25:04 -0700

    added new variables to config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 exports.PORT = process.env.PORT || 8080;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
 							global.TEST_DATABASE_URL ||
-							'mongodb://localhost/workout-logger-test-db';
+							'mongodb://cory:1101762@ds157987.mlab.com:57987/workout_logger_test';
