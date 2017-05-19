b106b506c8b39c85b1f8b9a5b7cf9563b6281513
cedmondson98311 <cedmondson98311@gmail.com>
2017-05-19 16:16:10 -0700

    finalized input validation rules and modified modal layouts

--- views/js/start_workout.js
+++ views/js/start_workout.js
@@ -26,15 +26,22 @@ function Set(){
 
 //STATE FUNCTIONS
 
-function addExercise(){
+function addExercise(state){
 	var exerciseName = $('#exercise').val();
-	var equipment = $('input[name=equipment]:checked', '#workout').val();
-	var category = $('input[name=category]:checked', '#workout').val();
+	var equipment = $('#equipment-selection option:selected').text();
+	var category = $('#category-selection option:selected').attr('data-value');
+	if(category === 'cardio') {
+		var exerciseDisplayName = exerciseName;
+	}
+	else {
+		var exerciseDisplayName = (equipment + ' ' + exerciseName);
+	};
 	var notes = $('#notes').val();
 	var id = generateId(exerciseName);
 	var newExercise = new Excercise();
 
 	newExercise.name = exerciseName;
+	newExercise.displayName = exerciseDisplayName;
 	newExercise.equipment = equipment;
 	newExercise.category = category;
 	newExercise.notes = notes;
@@ -42,9 +49,9 @@ function addExercise(){
 
 	state.workout.push(newExercise);
 	renderExercise(newExercise);
-}
+};
 
-function removeExercise(exerciseId){
+function removeExercise(state, exerciseId){
 	var index = findIndexById(exerciseId);
 
 	var newWorkout = [];
@@ -67,29 +74,55 @@ function removeExercise(exerciseId){
 	});
 }
 
-function addSet(index){
+function addSet(state, index){
 	var newSet = new Set();
+	var category = state.workout[index].category;
 
 	newSet.reps = $('#reps-input-modal').val();
 	newSet.weight = $('#weight-input-modal').val();
-	newSet.time= $('#time-input-modal').val();
+	newSet.time_hours = $('#time-input-modal-hours').val();
+	newSet.time_minutes = $('#time-input-modal-minutes').val();
+	newSet.time_seconds = $('#time-input-modal-seconds').val();
+	newSet.distance = $('#distance-input-modal').val();
+	newSet.speed = $('#speed-input-modal').val();
+	newSet.calories = $('#calories-input-modal').val();
+	if(category === 'cardio') {
+		newSet.distance_unit = $('.select-distance-units option:selected').attr('data-value');
+	};
 
-	var reps = $('#reps-input-modal').val();
-	var weight = $('#weight-input-modal').val();
-
-	var valid = validateInputSetsReps(reps,weight);
-	console.log('valid: ' + valid);
+	var validationResponse = validateInputSetsReps(newSet.reps, newSet.weight, newSet.time_hours, newSet.time_minutes, newSet.time_seconds, newSet.distance, newSet.speed, newSet.calories);
+	var valid = validationResponse['value'];
 
 	if(valid) {
-		state.workout[index].sets.push(newSet);
+		//This removes all null set values
+		var trimmedSet = {};
+		var keysArray = Object.keys(newSet);
+		keysArray.forEach(function(key) {
+			if(newSet[key] !== '') {
+				trimmedSet[key] = newSet[key];
+			}
+			else{};
+		});
+		state.workout[index].sets.push(trimmedSet);
 	}
 	else {
-		$('#bad-input-warning-div').removeClass('hidden');
-	}
-}
+		var cause = validationResponse['cause'];
+		switch(cause){
+			case('bad_digit'):
+				$('#bad-digit-warning-div').removeClass('hidden');
+				break;
+			case('negative_digit'):
+				$('#negative-digit-warning-div').removeClass('hidden');
+				break;
+			case('exceeded_sixty'):
+				$('#exceeded-sixty-warning-div').removeClass('hidden');
+				break;
+			default:
+		};
+	};
+};
 
-function removeSet(setIndex,exerciseId) {
-	console.log('target set index: ' + setIndex);
+function removeSet(state, setIndex, exerciseId) {
 	var wIndex = findIndexById(exerciseId);
 	var target = state.workout[wIndex].sets;
 	var newSets = [];
@@ -112,10 +145,11 @@ function removeSet(setIndex,exerciseId) {
 function renderExercise(exercise) {
 	var exerciseId = exercise.id;
 	var exerciseName = exercise.name;
+	var exerciseDisplayName = exercise.displayName;
 	var exerciseEquipment = exercise.equipment;
 	var exerciseNotes = exercise.notes;
 	
-	var rowHTML = '<tr><td class="exercise-title">' + exerciseName + '</td><td class="exercise-sets" data-id="' + exerciseId + '"></td>' + 
+	var rowHTML = '<tr><td class="exercise-title">' + exerciseDisplayName + '</td><td class="exercise-sets" data-id="' + exerciseId + '"></td>' + 
 		'<td><button type="button" data-name="' + exerciseName + '" data-id="' + exerciseId + '" class="btn btn-cir btn-sm btn-primary modify-set-button" data-toggle="modal" data-target="#add-set-modal">' + 
 		'<span class="glyphicon glyphicon-pencil"></span></button></td></tr>';
 
@@ -126,14 +160,10 @@ function renderSets(exerciseId) {
 	
 	var index = findIndexById(exerciseId);
 	var exerciseSets = state.workout[index].sets;
+	var category = state.workout[index].category;
 	
 	var setsHtml = exerciseSets.map(function(set) {
-		var reps = set.reps;
-		var weight = set.weight;
-
-		var html = reps + ' X ' + weight +'<br>';
-		
-		return html;
+		return generateSetsHtml(set, category);
 	});
 	
 	$('td[data-id|="' + exerciseId + '"]').html(setsHtml);
@@ -147,15 +177,17 @@ function renderSetsModal(exerciseId) {
 	 var target = state.workout[index];
 	 var name = target.name;
 	 var sets = target.sets;
+	 var category = target.category;
+	 var equipment = target.equipment;
+
+	 createModalTableHtml(category);
 
 	 var setsArray = []
 
 	 sets.forEach(function(set) {
 	 	var set_index = state.workout[index].sets.indexOf(set);
 	 	
-	 	var setHtml = '<tr><td>' + set.reps + '</td><td>' + set.weight + '</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
-
-		setsArray.push(setHtml);
+	 	setsArray.push(generateSetsHtmlModal(set, set_index, category, equipment, exerciseId));
 	 });
 	 
 	 var setsHtml = setsArray.join('');
@@ -167,9 +199,144 @@ function renderSetsModal(exerciseId) {
 	 $('#save-sets-modal').attr('data-index', index);
 	 $('#save-sets-modal').attr('data-id', exerciseId);
 	 $('#btn-confirm-delete').attr('data-id', exerciseId);
+
+
+	 revealModalInputs(category);
 };
 
 //HELPER FUNCTIONS
+
+function formatTime(set) {
+	var hr;
+	var min;
+	var sec;
+
+	if(set.time_hours && set.time_hours !== '') {
+		hr = set.time_hours;
+	}
+	else {
+		hr = '0';
+	};
+	if(set.time_minutes && set.time_minutes !== '') {
+		if(set.time_minutes.length > 1) {
+			min = set.time_minutes;
+	 	}
+	 	else {
+	 		min = '0' + set.time_minutes;
+	 	}
+	}
+	else {
+		min = '00';
+	};
+	if(set.time_seconds && set.time_seconds !== '') {
+		if(set.time_seconds.length > 1) {
+			sec = set.time_seconds;
+		}
+		else {
+			sec = '0' + set.time_seconds;
+		}
+	}
+	else {
+	sec = '00';
+	};
+
+	return hr + ':' + min + ':' + sec;
+}
+
+function generateSetsHtmlModal(set, set_index, category, equipment, exerciseId) {
+	var setHtml = '';
+	
+	switch(category) {
+		case('reps_and_weight'):
+			setHtml = '<tr><td>' + (set.reps || '-') + '</td><td>' + (set.weight || '-') + '</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+			break;
+		case('reps_only'):
+			if(equipment === "Body Weight") {
+			setHtml = '<tr><td>' + (set.reps || '-') + '</td><td>Body Weight</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+			break;
+			}
+			else{
+			setHtml = '<tr><td>' + (set.reps || '-') + '</td><td>-</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+			break;
+			}
+		case('time_only'):
+			var time = formatTime(set);
+			setHtml = '<tr><td>' + (time || '-') + '</td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+			break;
+		case('reps_and_time'):
+			var time = formatTime(set);
+			setHtml = '<tr><td>' + (set.reps || '-') + '</td><td>' + (time || '-') + '</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+			break;
+		case('cardio'):
+			var time = formatTime(set);
+			setHtml = '<tr><td>' + (time || '-') + '</td><td>' + (set.speed || '-') + '</td><td>' + (set.distance || '-') + '(' + set.distance_unit + ')</td><td>' + (set.calories || '-') + '</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+			break;
+		default:
+			setHtml = '<tr><td>' + (set.reps || '-') + '</td><td>' + (set.weight || '-') + '</td><td><button class="btn btn-sm btn-danger remove-set" data-id="' + exerciseId +'" data-set-index="' + set_index + '"><span class="glyphicon glyphicon-trash"></span></button></tr>';
+	};
+
+	return setHtml;
+}
+
+function generateSetsHtml(set, category) {
+	var html = '';
+
+		switch(category) {
+			case('reps_and_weight'):
+				html = (set.reps || '-') + ' X ' + (set.weight || '-') + '<br>';
+				break;
+			case('reps_only'):
+				if(set.weight) {
+					html = (set.reps || '-') + ' X ' + set.weight + '<br>'; 
+				}
+				else{
+					html = (set.reps || '-') + '<br>';
+				};
+				break;
+			case('time_only'):
+				var time = formatTime(set);
+				html = (time || '-') + '<br>';
+				break;
+			case('reps_and_time'):
+				var time = formatTime(set);
+				html = (set.reps || '-') + ' in ' + (time || '-') + '<br>'; 
+				break;
+			case('cardio'):
+				var time = formatTime(set);
+				var distance = '(' + (set.distance_unit || '-') + ')';
+				
+				html = 'Distance: ' + (set.distance || '-') + ' ' + (distance || '-') + '<br>Time: ' + (time || '-') + '<br>Speed: ' + (set.speed || '-') + '<br>Calories: ' + (set.calories || '-');
+				break;
+		};
+		
+		return html;
+	};
+
+function createModalTableHtml(category) {
+	var tableHtml = '';
+	
+	switch(category){
+		case('reps_and_weight'):
+			tableHtml = '<thead><tr><th>Reps</th><th>Weight</th></tr></thead><tbody id="logs-body-modal"></tbody>'
+			break;
+		case('reps_only'):
+			tableHtml = '<thead><tr><th>Reps</th><th>Weight</th></tr></thead><tbody id="logs-body-modal"></tbody>'
+			break;
+		case('time_only'):
+			tableHtml = '<thead><tr><th>Time</th></tr></thead><tbody id="logs-body-modal"></tbody>'
+			break;
+		case('reps_and_time'):
+			tableHtml = '<thead><tr><th>Reps</th><th>Time</th></tr></thead><tbody id="logs-body-modal"></tbody>'
+			break;
+		case('cardio'):
+			tableHtml = '<thead><tr><th>Time</th><th>Speed</th><th>Distance</th><th>Calories</th></tr></thead><tbody id="logs-body-modal"></tbody>'
+			break;
+		default:
+			tableHtml = '<thead><tr><th>Reps</th><th>Weight</th></tr></thead><tbody id="logs-body-modal"></tbody>'
+	};
+
+	$('#table-sets-modal').html(tableHtml);
+}
 
 function findIndexById(exerciseId) {
 	var index;
@@ -193,26 +360,64 @@ function generateId(exerciseName) {
   return exerciseName.toUpperCase().replace(/\s/g, '-') + ':' + idInteger;
 }
 
-
-function validateInputSetsReps(reps,weight) {
-	
-	var res = true;
-
-	for(var i = 0; i < reps.length; i ++) {
-		
-		if(isNaN(reps[i])) {
-			res = false;
-		} else {}
+function validateInputSetsReps(reps, weight, hr, min, sec, distance, speed, calories) {
+	var res = {
+		value:true,
+		cause:''
 	};
 
-	for(var s = 0; s < weight.length; s ++) {
+	if(isNaN(reps) || isNaN(weight) || isNaN(hr) || isNaN(min) || isNaN(sec) || isNaN(distance) || isNaN(speed) || isNaN(calories)) {
+		res['value'] = false;
+		res['cause'] = 'bad_digit';
+	}
+	else if ( Math.sign(reps) === -1 || Math.sign(weight) === -1 || Math.sign(hr) === -1 || Math.sign(min) === -1 || Math.sign(sec) === -1 || 
+		Math.sign(distance) === -1 || Math.sign(speed) === -1 || Math.sign(calories) === -1) {
 		
-		if(isNaN(weight[s])) {
-			res = false;
-		} else {}
-	};
+		res['value'] = false;
+		res['cause'] = 'negative_digit';
+	}
+	else if (min > 60 || sec > 60) {
+		res['value'] = false;
+		res['cause'] = 'exceeded_sixty';
+	}
+	else{};
 	
 	return res;
+};
+
+function revealModalInputs(category) {
+	$('#div-modal-input-reps').addClass('hidden');
+	$('#div-modal-input-weight').addClass('hidden');
+	$('#div-modal-input-time').addClass('hidden');
+	$('#div-modal-input-distance').addClass('hidden');
+	$('#div-modal-input-calories').addClass('hidden');
+	$('#div-modal-input-speed').addClass('hidden');
+
+	switch(category) {
+		case('reps_and_weight'):
+			$('#div-modal-input-reps').removeClass('hidden');
+			$('#div-modal-input-weight').removeClass('hidden');
+			break;
+		case('reps_only'):
+			$('#div-modal-input-reps').removeClass('hidden');
+			break;
+		case('time_only'):
+			$('#div-modal-input-time').removeClass('hidden');
+			break;
+		case('reps_and_time'):
+			$('#div-modal-input-reps').removeClass('hidden');
+			$('#div-modal-input-time').removeClass('hidden');
+			break;
+		case('cardio'):
+			$('#div-modal-input-time').removeClass('hidden');
+			$('#div-modal-input-distance').removeClass('hidden');
+			$('#div-modal-input-calories').removeClass('hidden');
+			$('#div-modal-input-speed').removeClass('hidden');
+			break;
+		default:
+			$('#div-modal-input-reps').removeClass('hidden');
+			$('#div-modal-input-weight').removeClass('hidden');
+	};
 };
 
 //API FUNCTIONS
@@ -224,13 +429,13 @@ $(function() {
 		//Add Exercise Click
 		$('#add-exercise-button').click(function(e) {
 			e.preventDefault();
-			addExercise();
+			addExercise(state);
 		});
 
 		//Add Exercise Submit
 		$('#add-exercise-form').submit(function(e) {
 			e.preventDefault();
-			addExercise();
+			addExercise(state);
 		});
 
 	//Remove Exercise
@@ -248,7 +453,7 @@ $(function() {
 			$('#logs-body').html('');
 
 			var exerciseId = $(this).attr('data-id');
-			removeExercise(exerciseId);
+			removeExercise(state, exerciseId);
 
 			$('#delete-warning-div').addClass('hidden');
 		});
@@ -262,17 +467,18 @@ $(function() {
 		renderSetsModal(exercise);
 	});
 
-	//Create Sets Click
+	//Create Sets By Click Event
 	$('#add-set-button-modal').click(function(e) {
 		e.preventDefault();
 
 		var index = $(this).attr('data-index');
 		var exerciseId = $(this).attr('data-id');
 
-		addSet(index);
+		addSet(state, index);
 		renderSetsModal(exerciseId);
 	});
 
+	//Create Sets By Submit Event
 	$('#modal-form').submit(function(e) {
 		e.preventDefault();
 		console.log('fired');
@@ -281,29 +487,39 @@ $(function() {
 		console.log(index);
 		var exerciseId = $(this).find('.add-set-button-modal').attr('data-id');
 
-		addSet(index);
-		renderSetsModal(exerciseId)
-	})
+		addSet(state, index);
+		renderSetsModal(exerciseId);
+	});
 
 	//Save Sets
 	$('#save-sets-modal').click(function(e) {
 		var exerciseId = $(this).attr('data-id');
 		renderSets(exerciseId);
+		$('#modal-form')[0].reset();
 
 	});
 
 	//Remove Set
-	$('#logs-body-modal').on('click','.remove-set', function(e) {
+	$('#table-sets-modal').on('click','.remove-set', function(e) {
+		console.log('listener fired');
 		var setIndex = $(this).attr('data-set-index');
 		var exerciseId = $(this).attr('data-id');
 		
-		removeSet(setIndex,exerciseId);
+		removeSet(state, setIndex, exerciseId);
 	});
 
 	//Acknowledge Sets/Reps Input Error
-	$('.btn-notify-bad-input').click(function(e) {
+	$('.btn-notify-bad-digit').click(function(e) {
 		e.preventDefault();
-		$('#bad-input-warning-div').addClass('hidden');
+		$('#bad-digit-warning-div').addClass('hidden');
+	});
+	$('.btn-notify-negative-digit').click(function(e) {
+		e.preventDefault();
+		$('#negative-digit-warning-div').addClass('hidden');
+	});
+	$('.btn-notify-exceeded-sixty').click(function(e) {
+		e.preventDefault();
+		$('#exceeded-sixty-warning-div').addClass('hidden');
 	});
 });
 
